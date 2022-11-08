61b6b78f83de5531c281670bc63e2664929dffa8
GabrielaFelder <gab.felder@bluewin.ch>
2022-11-08 19:48:39 +0800

    seed the db with articles and content

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Article.destroy_all
+
+10.times do
+  article = Article.create(title: Faker::Game.title, content: Faker::Lorem.paragraph(sentence_count: 3))
+  puts article.title
+end
